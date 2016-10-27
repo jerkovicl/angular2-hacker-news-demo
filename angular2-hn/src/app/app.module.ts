e544b809e60944ab1c67bdf68c5f1c9a451e2a60
Luka Jerković <luka.jerkovic@sistemi.hr>
2016-10-27 23:06:31 +0200

    added HackerNews service component

--- angular2-hn/src/app/app.module.ts
+++ angular2-hn/src/app/app.module.ts
@@ -3,20 +3,30 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { StoriesComponent } from './stories/stories.component';
+import { FooterComponent } from './footer/footer.component';
+import { ItemComponent } from './item/item.component';
+import { HackerNewsAPIService } from './hackernews-api.service';
+import { DomainPipe } from './domain.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    StoriesComponent,
+    FooterComponent,
+    ItemComponent,
+    DomainPipe
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [HackerNewsAPIService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
